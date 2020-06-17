efc98ce6c61ac22c8d8854635b0cd4923197fb06
Alice <agenkin1@gmail.com>
2020-06-17 01:25:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,9 +7,9 @@ return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(integer)
-   while integer > 0 
+   while integer > 0
     sleep (1.seconds)
-    puts "#{integer} SECOND(S)!" 
+    puts "#{integer} SECOND(S)!"
     integer-=1
 end
  puts "HAPPY NEW YEAR!"
